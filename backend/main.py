09c1c3282f61f7c85d65a115566925f71a55d0a4
nikitaCode09-ops <nikitakumari28092006@gmail.com>
2026-09-02 23:09:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -91,8 +91,8 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
